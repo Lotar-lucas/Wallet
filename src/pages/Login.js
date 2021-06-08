3818a98b56a3be2d07e0894f03dc4ee0501fe286
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-06-08 16:30:33 -0300

    form de login criado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              name="email"
+              data-testid="email-input"
+              required
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              name="password"
+              data-testid="password-input"
+              minLength="6"
+              required
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+      </main>
+
+    );
   }
 }
 
