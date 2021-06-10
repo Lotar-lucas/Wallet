468babcef0df07e0ce7f6d62ba36c38db324017c
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-06-10 13:21:58 -0300

    importado connect e mapState para o Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return; (
-      <header> 
-        
-      <header/>
+    const { email } = this.props;
+    return (
+      <header>
+        <nav>
+          <h1>Welcome to your wallet</h1>
+          <p>
+            Seu emial:
+            {' '}
+            {email}
+          </p>
+        </nav>
+      </header>
 
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
